b6d4779e11f4d33fb103f15116bdfed22ad4c35e
Jarek Foksa <jarek@foksa.name>
2024-05-02 19:06:22 +0200

    Use spaces instead of tabs

--- types.d.ts
+++ types.d.ts
@@ -1,45 +1,45 @@
 interface SVGPathElement {
-	/**
-	 * See https://github.com/jarek-foksa/path-data-polyfill
-	 *
-	 * See https://svgwg.org/specs/paths/#InterfaceSVGPathData
-	 */
-	getPathData: (settings?: SVGPathDataSettings) => Array<SVGPathSegment>;
-	/**
-	 * See https://github.com/jarek-foksa/path-data-polyfill
-	 *
-	 * See https://svgwg.org/specs/paths/#InterfaceSVGPathData
-	 */
-	setPathData: (pathData: Array<SVGPathSegment>) => void;
+  /**
+   * See https://github.com/jarek-foksa/path-data-polyfill
+   *
+   * See https://svgwg.org/specs/paths/#InterfaceSVGPathData
+   */
+  getPathData: (settings?: SVGPathDataSettings) => Array<SVGPathSegment>;
+  /**
+   * See https://github.com/jarek-foksa/path-data-polyfill
+   *
+   * See https://svgwg.org/specs/paths/#InterfaceSVGPathData
+   */
+  setPathData: (pathData: Array<SVGPathSegment>) => void;
 }
 
 type SVGPathDataCommand =
-	| "A"
-	| "a"
-	| "C"
-	| "c"
-	| "H"
-	| "h"
-	| "L"
-	| "l"
-	| "M"
-	| "m"
-	| "Q"
-	| "q"
-	| "S"
-	| "s"
-	| "T"
-	| "t"
-	| "V"
-	| "v"
-	| "Z"
-	| "z";
+  | "A"
+  | "a"
+  | "C"
+  | "c"
+  | "H"
+  | "h"
+  | "L"
+  | "l"
+  | "M"
+  | "m"
+  | "Q"
+  | "q"
+  | "S"
+  | "s"
+  | "T"
+  | "t"
+  | "V"
+  | "v"
+  | "Z"
+  | "z";
 
 interface SVGPathDataSettings {
-	normalize: boolean;
+  normalize: boolean;
 }
 
 interface SVGPathSegment {
-	type: SVGPathDataCommand;
-	values: Array<number>;
+  type: SVGPathDataCommand;
+  values: Array<number>;
 }
