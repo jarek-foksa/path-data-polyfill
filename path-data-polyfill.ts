0e9627279586b821b568455e949d1090cad0ae30
RobertAKARobin <contact@robertakarobin.com>
2024-05-02 09:45:32 -0500

    Add Typescript typings

---
+++ path-data-polyfill.ts
@@ -0,0 +1,1 @@
+export * from './path-data-polyfill.js';
---
+++ types.d.ts
@@ -0,0 +1,49 @@
+interface SVGPathElement {
+	/**
+	 * See https://github.com/jarek-foksa/path-data-polyfill
+	 *
+	 * See https://svgwg.org/specs/paths/#InterfaceSVGPathData
+	 */
+	getPathData: (settings?: SVGPathDataSettings) => Array<SVGPathSegment>;
+	/**
+	 * See https://github.com/jarek-foksa/path-data-polyfill
+	 *
+	 * See https://svgwg.org/specs/paths/#InterfaceSVGPathData
+	 */
+	setPathData: (pathData: Array<SVGPathSegment>) => void;
+}
+
+type SVGPathDataCommand =
+	| `A`
+	| `a`
+	| `B`
+	| `b`
+	| `C`
+	| `c`
+	| `H`
+	| `h`
+	| `L`
+	| `l`
+	| `M`
+	| `m`
+	| `Q`
+	| `q`
+	| `R`
+	| `r`
+	| `S`
+	| `s`
+	| `T`
+	| `t`
+	| `V`
+	| `v`
+	| `Z`
+	| `z`;
+
+interface SVGPathDataSettings {
+	normalize: boolean;
+}
+
+interface SVGPathSegment {
+	type: SVGPathDataCommand;
+	values: Array<number>;
+}
